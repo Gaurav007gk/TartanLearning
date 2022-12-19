8552fe66021971d4cab629e6fe5bed73de2b6562
Gaurav007gk <gaurav9797kr@gmail.com>
2022-12-20 03:29:07 +0530

    first commit

---
+++ src/StartJava.java
@@ -0,0 +1,10 @@
+public class StartJava {
+
+    public static void main(String args[]) {
+         System.out.println("hello");
+        System.out.println(10+10);
+        String s="Game";
+        System.out.println(s);
+
+        }
+}
