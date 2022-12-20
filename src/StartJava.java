19d2cf11df6b4a8d850606fd035b8e4290fd19ac
Gaurav007gk <gaurav9797kr@gmail.com>
2022-12-20 15:21:29 +0530

    first commit

--- src/StartJava.java
+++ src/StartJava.java
@@ -5,8 +5,23 @@ public class StartJava {
         System.out.println(10+10);
         String s="Game";
         System.out.println(s);
-
-
+        int number1=20;
+        int number2=40;
+        int res=number1+number2;
+        System.out.println(res);
+          double pi=3.14;
+          boolean isAdult= true;
+          char c='a';
+          int numberOne=30;
+          int numberTwo=10;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        int num=1;
+        System.out.println(num++);
+        System.out.println(--num);
+        System.out.println(++num);
 
         }
 }
